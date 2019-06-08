9e549e5f6632d0da52270fdf7f2fc74cbf6c7cc5
jdrphillips <me@jdrphillips.io>
2019-06-08 18:36:53 +0100

    Initial project + Symbol definition

---
+++ src/main/scala/io/typechecked/symbology/Symbol.scala
@@ -0,0 +1,67 @@
+package io.typechecked
+package symbology
+
+sealed trait Symbol
+
+object Symbol {
+
+  trait _0 extends Symbol
+  trait _1 extends Symbol
+  trait _2 extends Symbol
+  trait _3 extends Symbol
+  trait _4 extends Symbol
+  trait _5 extends Symbol
+  trait _6 extends Symbol
+  trait _7 extends Symbol
+  trait _8 extends Symbol
+  trait _9 extends Symbol
+  trait _10 extends Symbol
+  trait _11 extends Symbol
+  trait _12 extends Symbol
+  trait _13 extends Symbol
+  trait _14 extends Symbol
+  trait _15 extends Symbol
+  trait _16 extends Symbol
+  trait _17 extends Symbol
+  trait _18 extends Symbol
+  trait _19 extends Symbol
+  trait _20 extends Symbol
+  trait _21 extends Symbol
+  trait _22 extends Symbol
+  trait _23 extends Symbol
+  trait _24 extends Symbol
+  trait _25 extends Symbol
+  trait _28 extends Symbol
+  trait _29 extends Symbol
+  trait _37 extends Symbol
+  trait _46 extends Symbol
+  trait _55 extends Symbol
+  trait _57 extends Symbol
+  trait _71 extends Symbol
+  trait _75 extends Symbol
+  trait _86 extends Symbol
+  trait _113 extends Symbol
+  trait _140 extends Symbol
+  trait _144 extends Symbol
+  trait _171 extends Symbol
+  trait _173 extends Symbol
+  trait _227 extends Symbol
+  trait _259 extends Symbol
+  trait _421 extends Symbol
+  trait _514 extends Symbol
+  trait _777 extends Symbol
+  trait _1543 extends Symbol
+  trait _2333 extends Symbol
+  trait _48611 extends Symbol
+  trait _15485863 extends Symbol
+  trait _141214768240 extends Symbol
+  trait _282429536480 extends Symbol
+  trait _423644304720 extends Symbol
+  trait _847288609442 extends Symbol
+  trait _1129718145922 extends Symbol
+  trait _617673396283946 extends Symbol
+  trait _1853020188851840 extends Symbol
+  trait _2470693585135786 extends Symbol
+  trait _2470693585135787 extends Symbol
+
+}
